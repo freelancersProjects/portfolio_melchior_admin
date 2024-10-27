0b7653125d71b39fc9f65f10bcf7cb0e5e6d3178
Titi <tristanfio05@gmail.com>
2024-10-27 19:45:50 +0100

    Display the editing form by clicking on the content button

--- src/Controller/Admin/ContentCrudController.php
+++ src/Controller/Admin/ContentCrudController.php
@@ -7,13 +7,46 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ContentCrudController extends AbstractCrudController
 {
+    private AdminUrlGenerator $adminUrlGenerator;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->entityManager = $entityManager;
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Content::class;
+    }
+
+    #[Route('/admin/content_redirect', name: 'admin_content_redirect')]
+    public function redirectToContentForm(): Response
+    {
+        $content = $this->entityManager->getRepository(Content::class)->findOneBy([]);
+
+        if (!$content) {
+            $content = new Content();
+            $this->entityManager->persist($content);
+            $this->entityManager->flush();
+        }
+
+        $url = $this->adminUrlGenerator
+            ->setController(self::class)
+            ->setAction('edit')
+            ->setEntityId($content->getId())
+            ->generateUrl();
+
+        return $this->redirect($url);
     }
 
     public function configureFields(string $pageName): iterable
